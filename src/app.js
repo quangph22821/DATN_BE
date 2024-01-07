988b525e2b9aa9602e6687de7c42bc3ef7885f09
luontd <110533951+luontd@users.noreply.github.com>
2024-01-07 14:03:39 +0700

    authen vào router

--- src/app.js
+++ src/app.js
@@ -2,13 +2,13 @@ import express from "express";
 import commentRouter from "./routes/comment"
 import productRouter from "./routes/product";
 import authRouter from "./routes/auth";
-import categoryRouter from "./routes/category";
-import cartRouter from "./routes/cart";
+// import categoryRouter from "./routes/category";
+// import cartRouter from "./routes/cart";
 import materialRouter from "./routes/material";
 import originRouter from "./routes/origin";
 import userRouter from "./routes/user";
 import staticRouter from"./routes/static"
-import billRouter from"./routes/bill"
+// import billRouter from"./routes/bill"
 import cors from "cors"
 import mongoose from "mongoose";
 
@@ -20,13 +20,13 @@ app.use(express.json());
 
 // router
 app.use("", productRouter);
-app.use("", categoryRouter);
+// app.use("", categoryRouter);
 app.use("", authRouter);
-app.use("", cartRouter);
+// app.use("", cartRouter);
 app.use("", materialRouter);
 app.use("", originRouter);
 app.use("", userRouter);
-app.use("", billRouter);
+// app.use("", billRouter);
 app.use("", commentRouter)
 app.use("", staticRouter);
 
