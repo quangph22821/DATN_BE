6e0c4bf4af0711af83c89d85f18c17428da7cfb8
quangph22821 <hoangquang18022003@gmail.com>
2024-01-08 22:41:18 +0700

    models cart

--- src/models/cart.js
+++ src/models/cart.js
@@ -1,19 +1,22 @@
-import mongoose from "mongoose";
+import mongoose, { Types } from "mongoose";
 
 const cartSchema = new mongoose.Schema(
     {
-        userId: {
-            type: mongoose.Types.ObjectId, ref: "User"
+      userId: { type: Types.ObjectId, ref: "User" },
+      products: [
+        {
+          productId: { type: Types.ObjectId, ref: "Product" },
+          quantity: Number,
+          price: Number,
         },
-        products: [
-            {
-                productId: { type: mongoose.Types.ObjectId, ref: "Product", },
-                quantity: Number,
-                // price: Number,
-            },
-        ],
+      ],
+      shippingFee: Number,
+      totalPrice: Number,
+      totalOrder: Number,
+      createdAt: Date,
+      updatedAt: Date,
     },
-    { timestamps: true, versionKey: false }
-);
+    { versionKey: false, timestamps: true }
+  );
 
 export default mongoose.model("Cart", cartSchema);
