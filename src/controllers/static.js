d11e0fda1c9834887d4b2f78e64f49e7c0b19a36
Minhquy3112 <joshquy2003@gmail.com>
2024-01-16 23:55:24 +0700

    thống kê theo

--- src/controllers/static.js
+++ src/controllers/static.js
@@ -1,88 +1,9 @@
 import Bill from "../models/bill";
+import moment from "moment";
 
-// lấy tất cả đơn hàng
-export const getToalPrice = async (req, res) => {
+
+const aggregateTotalPrice = async (matchExpression) => {
   try {
-    let bills = await Bill.find({});
-    //   console.log(bills);
-    if (!bills || bills.length === 0) {
-      return res.status(400).json({ message: "Không có hóa đơn!" });
-    }
-    let formattedBills = bills.map((bill) => ({
-      totalPrice: bill.totalPrice,
-      month: bill.createdAt.getMonth() + 1,
-      date: bill.createdAt.getDate(),
-      year: bill.createdAt.getFullYear(),
-      // Thêm các trường khác nếu cần
-    }));
-    // console.log(formattedBills);
-    let totalPrice = 0;
-    for (let i = 0; i < bills.length; i++) {
-      totalPrice += bills[i].totalPrice;
-    }
-    // console.log(totalPrice);
-    return res
-      .status(200)
-      .json({ message: "Lấy danh sách bill thành công!", bills });
-  } catch (error) {
-    return res.status(500).json({
-      message: error.message,
-    });
-  }
-};
-
-
-export const getTotalPriceByFilter = async (req, res) => {
-  try {
-    const { year, month, week, day } = req.query;
-
-    let matchExpression = {};
-
-    if (year) {
-      matchExpression.$expr = { $eq: [{ $year: "$createdAt" }, parseInt(year)] };
-    }
-
-    if (month) {
-      matchExpression.$expr = {
-        $and: [
-          { $eq: [{ $year: "$createdAt" }, parseInt(year)] },
-          { $eq: [{ $month: "$createdAt" }, parseInt(month)] },
-        ],
-      };
-    }
-
-    if (week) {
-      if (day) {
-        // Lọc dữ liệu theo tuần hiện tại và đến ngày hiện tại
-        matchExpression.$expr = {
-          $and: [
-            { $eq: [{ $year: "$createdAt" }, parseInt(year)] },
-            { $eq: [{ $week: "$createdAt" }, parseInt(week)] },
-            { $gte: ["$createdAt", new Date(`${year}-${month}-01`)] }, // Ngày đầu tiên của tháng
-            { $lte: ["$createdAt", new Date(`${year}-${month}-31T23:59:59`)] }, // Ngày cuối cùng của tháng
-          ],
-        };
-      } else {
-        // Lọc dữ liệu theo tuần hiện tại mà không yêu cầu ngày cụ thể
-        matchExpression.$expr = {
-          $and: [
-            { $eq: [{ $year: "$createdAt" }, parseInt(year)] },
-            { $eq: [{ $week: "$createdAt" }, parseInt(week)] },
-          ],
-        };
-      }
-    }
-
-    if (day) {
-      matchExpression.$expr = {
-        $and: [
-          { $eq: [{ $year: "$createdAt" }, parseInt(year)] },
-          { $eq: [{ $month: "$createdAt" }, parseInt(month)] },
-          { $eq: [{ $dayOfMonth: "$createdAt" }, parseInt(day)] },
-        ],
-      };
-    }
-
     const aggregatedResults = await Bill.aggregate([
       {
         $match: matchExpression,
@@ -96,15 +17,39 @@ export const getTotalPriceByFilter = async (req, res) => {
     ]);
 
     if (!aggregatedResults || aggregatedResults.length === 0) {
-      return res.status(400).json({
-        message: "Không có dữ liệu thỏa mãn điều kiện lọc!",
-      });
+      return 0;
     }
 
-    const totalPrice = aggregatedResults[0].total;
+    return aggregatedResults[0].total;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const getTotalPriceByDate = async (req, res) => {
+  try {
+    const { selectedYear, selectedMonth, selectedDay } = req.body;
+
+    if (!selectedYear || !selectedMonth || !selectedDay) {
+      return res
+        .status(400)
+        .json({ message: "Vui lòng chọn năm, tháng và ngày!" });
+    }
+
+    const matchExpression = {
+      $expr: {
+        $and: [
+          { $eq: [{ $year: "$createdAt" }, parseInt(selectedYear)] },
+          { $eq: [{ $month: "$createdAt" }, parseInt(selectedMonth)] },
+          { $eq: [{ $dayOfMonth: "$createdAt" }, parseInt(selectedDay)] },
+        ],
+      },
+    };
+
+    const totalPrice = await aggregateTotalPrice(matchExpression);
 
     return res.status(200).json({
-      message: "Lọc dữ liệu thành công!",
+      message: `Lấy tổng giá trị cho ngày ${selectedDay}/${selectedMonth}/${selectedYear} thành công!`,
       totalPrice,
     });
   } catch (error) {
