6fb490bf7db4d366c41a65afa12831bd384b2eb0
luontd <110533951+luontd@users.noreply.github.com>
2024-01-16 18:15:52 +0700

    sửa lỗi

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,9 +1,6 @@
 import express from "express";
-
 // import { checkPermission } from "../middlewares/checkPermission";
-
 import { getAll,remove,update, getUserProfile } from "../controllers/user";
-
 // import { authenticate } from "../middlewares/authenticate";
 
 const router = express.Router();
